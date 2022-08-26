10731e27f5869ac3d091c1e7254c4a3d83f65b22
verhovensky <voinywolnyprod@gmail.com>
2022-08-27 00:29:37 +0300

    models.py minor fix

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
-    body = models.TextField(max_length=255, verbose_name="Содержание")
+    body = models.TextField(max_length=5000, verbose_name="Содержание")
     date = models.DateTimeField(verbose_name="Дата публикации")
     slug = models.SlugField(max_length=255, allow_unicode=True, verbose_name="Слаг")
     tags = TaggableManager(verbose_name="Тэги")
