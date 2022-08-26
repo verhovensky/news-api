8f77f553c4d1e925f3191555d4aac51274bc3a81
verhovensky <voinywolnyprod@gmail.com>
2022-08-27 00:25:23 +0300

    parse_news.py command for posts creation in db

--- news/management/commands/parse_news.py
+++ news/management/commands/parse_news.py
@@ -1,21 +1,47 @@
-from django.core.management.base import BaseCommand, CommandError
-from news.models import Post
+from django.core.management.base import BaseCommand
+from news.serializers import PostREADSerializer
+from django.utils.text import slugify
+from datetime import datetime
+from .yoparser import parse_news_links_ozon, parse_news_links_yandex, \
+    parse_single_article_link_yandex, parse_single_article_link_ozon
 
 
 class Command(BaseCommand):
-    help = 'Parse news from Yandex and Ozon'
-
-    # def add_arguments(self, parser):
-    #     parser.add_argument('verbose', nargs='+', type=bool)
+    help = "Parse news from Yandex and Ozon"
 
     def handle(self, *args, **options):
-        for poll_id in options['poll_ids']:
-            try:
-                poll = Post.objects.get(pk=poll_id)
-            except Post.DoesNotExist:
-                raise CommandError('Poll "%s" does not exist' % poll_id)
-
-            poll.opened = False
-            poll.save()
-
-            self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+        # TODO: try except (KeyError)
+        self.stdout.write(self.style.SUCCESS("Start parsing..."))
+        ozon = parse_news_links_ozon()
+        for i in ozon.items():
+            b = parse_single_article_link_ozon(i[1]["link"])
+            date = datetime.strptime(i[1]["date"].strip("Z"),
+                                     "%Y-%m-%dT%H:%M:%S.%f")
+            serializer = PostREADSerializer(
+                data={"title": i[1]["title"],
+                      "body": b["body"],
+                      "date": date,
+                      "slug": i[1]["slug"],
+                      "tags": i[1]["tags"]})
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                self.stdout.write(f"Data not valid: {serializer.errors}")
+        self.stdout.write(self.style.SUCCESS(f"Parsed and loaded {len(ozon)} Ozon news"))
+        yandex = parse_news_links_yandex()
+        for i in yandex.items():
+            b = parse_single_article_link_yandex(i[1])
+            date = datetime.strptime(b["date"].strip('+03:00'),
+                                     "%Y-%m-%dT%H:%M:%S")
+            slug = slugify(b["title"], allow_unicode=True)
+            serializer = PostREADSerializer(
+                data={"title": b["title"],
+                      "body": b["body"],
+                      "date": date,
+                      "slug": slug,
+                      "tags": b["tags"]})
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                self.stdout.write(f"Data not valid: {serializer.errors}")
+        self.stdout.write(self.style.SUCCESS(f"Parsed and loaded {len(yandex)} Yandex news"))
