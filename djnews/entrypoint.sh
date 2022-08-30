aa5c69bc45cabed1418351b34bccd6d4a878cd9c
verhovensky <voinywolnyprod@gmail.com>
2022-08-30 11:54:45 +0300

    superuser creation on startup add

--- djnews/entrypoint.sh
+++ djnews/entrypoint.sh
@@ -7,6 +7,8 @@ if [ "$1" = "run_django" ]; then
   python manage.py makemigrations --no-input
   python manage.py migrate --no-input
   python manage.py collectstatic --no-input
+  python manage.py shell -c \
+  "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
   python manage.py test
   exec gunicorn parsernews.wsgi:application -b 0.0.0.0:8000 --reload
 fi
